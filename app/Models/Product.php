f0aaa20ebf7ee4b3ef96bb011f37094b97f805b8
Uchida <uhida252637@gmail.com>
2022-06-05 18:21:34 +0900

    Productモデルfillableの修正

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,9 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title', 'maker', 'description', 'color',
+                            'size', 'image', 'price'];
+
     public function register()
     {
         return $this->hasmany(Order::class);
