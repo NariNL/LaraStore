255f6f1a929b9d2f99042474073c86048255aa91
Uchida <uhida252637@gmail.com>
2022-06-05 15:18:34 +0900

    products テーブルのマイグレーション作成

---
+++ database/migrations/2022_05_29_050718_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('maker', 100);
+            $table->string('description', 500);
+            $table->string('color', 50);
+            $table->string('size', 50);
+            $table->string('image', 500);
+            $table->integer('price');
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
