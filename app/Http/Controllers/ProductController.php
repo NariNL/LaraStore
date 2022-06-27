ee263a399f0bf32eb6e574eeccfa5764dee7c836
gon <zukazuka555222@gmail.com>
2022-06-27 20:30:02 +0900

    編集

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -102,15 +102,16 @@ class ProductController extends Controller
     {
         $product = Product::where('id', '=', $request->id)->first();
         // $products = Stock::join("紐づける対象のテーブル","テーブル名.対象のカラム","テーブル名.対象のカラム名")->get()->toArray();
-        $products = Stock::join("order_detail","stock.product_id","order_detail.item_id")->get()->toArray();
-        $item_sales_quantity = 0;
-        $item_inventory = 0;
-        foreach($products as $value)
-        {
-            $item_sales_quantity = $item_sales_quantity + $value->sales_quantity;
-            $item_inventory = $item_inventory + $value->inventory; 
-        }
-        $stock_inventory =  $item_inventory - $item_sales_quantity;
+        // $products = Stock::join("order_detail","stock.product_id","order_detail.item_id")->get()->toArray();
+        // $item_sales_quantity = 0;
+        // $item_inventory = 0;
+        // foreach($products as $value)
+        // {
+        //     $item_sales_quantity = $item_sales_quantity + $value->sales_quantity;
+        //     $item_inventory = $item_inventory + $value->inventory; 
+        // }
+        // $stock_inventory =  $item_inventory - $item_sales_quantity;
+        $stock_inventory = "1";
         return view('products.detail', [
             'product' => $product,
             'stock_inventory' => $stock_inventory
