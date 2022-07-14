ec3df98ae35346081f01667c817a4a7932040033
houzichaoisii <sasoayu.s@icloud.com>
2022-07-14 19:24:54 +0900

    商品詳細画面の変更

--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -4,6 +4,13 @@
     <div class="container">
     <div class="panel-body">
         <td class="">
+        </td>
+
+    </div>
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="card-text">            
             <div>商品名：<strong>{{$product->title}}</strong></div>
             <div>製造元：<strong>{{$product->maker}}</strong></div>
             <div>商品説明：<strong>{{$product->description}}</strong></div>
@@ -12,10 +19,10 @@
             <div>{{$product->image}}</div>
             <div>在庫数:<strong>{{$stock_inventory}}</strong></div>
             <div>値段：<strong>{{$product->price}}</strong></div>
-            <a href="/product/edit/{{$product->id}}">編集</a>
-            <a href="/products">一覧に戻る</a>
-        </td>
-
+        </p>
+        <a href="/product/edit/{{$product->id}}" class="btn btn-info">編集</a>
+        <a href="/products" class="btn btn-primary">一覧に戻る</a>
+    </div>
     </div>
     </div>
 @endsection
