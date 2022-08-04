46b25e4ecdc2c67d1b4b9a06fc8972b762519de1
syoki.isihira <zenkoku33@gmail.com>
2022-08-04 16:48:34 +0900

    ログイン後のURL変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/products/visitor';
 
     /**
      * Create a new controller instance.
